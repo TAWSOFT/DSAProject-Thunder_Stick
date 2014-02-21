9b0cade77502cf40a2418d521cb08ec5cb91cb33
tawaasalage <tawaasalage@gmail.com>
2014-02-21 11:33:51 +0530

    Constructors Added
    Default Constructor and Parameterized Constructor added to the BSTNode class

--- src/com/DSA/BinerySearch/BSTNode.java
+++ src/com/DSA/BinerySearch/BSTNode.java
@@ -11,4 +11,25 @@ class BSTNode
     String Author_Name;
     String Author_Surname;
 
+ public BSTNode()
+	{
+        left = null;
+        right = null;
+        ISBN= 0;
+        Name="";
+        BType="";
+        Author_Name="";
+        Author_Surname="";
+    }
+public BSTNode(int INo,String na,String n3,String n4,String n5)
+    {
+        left = null;
+        right = null;
+        ISBN = INo;
+        Name=na;
+        BType=n5;
+        Author_Name=n3;
+        Author_Surname=n4;
+    }
+
 }
