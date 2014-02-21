3a1b7ebbb080551587b95f6912c7d4339a325887
tawaasalage <tawaasalage@gmail.com>
2014-02-21 11:26:02 +0530

    Binary Search Tree Node File Added

---
+++ src/com/DSA/BinerySearch/BSTNode.java
@@ -0,0 +1,13 @@
+package com.DSA.BinerySearch;
+
+
+class BSTNode
+{
+    BSTNode left, right;
+    int ISBN;
+    String Name;
+    String BType;
+    String Author_Name;
+    String Author_Surname;
+
+}
