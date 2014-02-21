c5da5a60138f43a95609d4d0de5475168143906c
kaushi90 <kanishka.kaushi@gmail.com>
2014-02-21 12:15:49 +0530

    Insert Function Added to the BST file
    Insert function directly communicating with the GUI

--- src/com/DSA/BinerySearch/BST.java
+++ src/com/DSA/BinerySearch/BST.java
@@ -6,5 +6,9 @@ class BST
 	private BSTNode root;
     public BSTNode Tmproot;
     boolean availability;
+	
+	public void insert(int data,String name,String A_Name,String Sur,String Title)
+    {
+	}
      
 }
